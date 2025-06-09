01dd76ba10c057fcae134962a1ead74ea96e1a40
241019 <241019@std.hi-joho.ac.jp>
2025-06-09 12:27:51 +0900

    チカレタ...

--- src/Hero.java
+++ src/Hero.java
@@ -1,20 +1,38 @@
 public class Hero {
     String name;
     int hp;
+    Sword sword;
+    public void attack() {
+        System.out.println(this.name + "は攻撃した！");
+        System.out.println("敵に5ポイントのダメージをあたえた！");
+    }
+    // 引数あり
+    public Hero(String name) {
+        this.hp = 100;
+        this.name = name;
+    }
+    // 引数なし
+    public Hero() {
+        this("ダミー");
+    }
+
     public void sleep() {
         this.hp = 100;
         System.out.println(this.name + "は眠って回復した！");
     }
+
     public void sit(int sec) {
         this.hp += sec;
         System.out.println(this.name + "は" + sec + "秒座った！");
         System.out.println("HPが" + sec + "回復した！");
     }
+
     public void slip() {
         this.hp -= 5;
         System.out.println(this.name + "は転んだ！");
         System.out.println("5のダメージ！");
     }
+
     public void run() {
         System.out.println(this.name + "は逃げ出した！");
         System.out.println("gameover");
